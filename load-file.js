251acf580bb2be2c686045ae819e3183ff487934
Lucas Marques <lucaskauz@gmail.com>
2021-02-25 09:02:18 -0300

    feat: add load-file

---
+++ load-file.js
@@ -0,0 +1,8 @@
+// This is an example
+var fs = require('fs');
+fs.readFile( __dirname + '/test.txt', function (err, data) {
+  if (err) {
+    throw err; 
+  }
+  console.log(data.toString());
+});
